4937c00070ff7777348772c13ea59c0cd86c7aa9
Dane M Goodman <DaneGoodman85@gmail.com>
2020-07-18 15:55:55 -0700

    Added Bootstrap and a div to the Home component

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -3,13 +3,27 @@ import './Home.css';
 
 class Home extends Component {
     state = {
-
+        recipe: null
     }
+
+    componentDidMount() {
+        fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                this.setState({ recipe: data });
+            }
+            )
+    };
 
     render() {
         return(
             <div>
-                Hello World
+                <div id="meal-search-content">
+                    <h2>Welcome to the random meal generator</h2>
+                    <p>Have a hungry family to feed but don't know what to make for dinner?</p>
+                    <p>Simply give our random meal generator button a click and prepare to be inspired!</p>
+                </div>
             </div>
         )
     }
