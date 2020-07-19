7e57de7ad33345c911f907b758f2b3d32af56a25
Dane M Goodman <DaneGoodman85@gmail.com>
2020-07-18 22:19:30 -0700

    Button works to generate random meal ideas, so far the title of the dish and the image are rendered, tomorrow I need to figure out how to set state to populate an array

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -3,18 +3,47 @@ import './Home.css';
 
 class Home extends Component {
     state = {
-        recipe: null
+        recipe: null,
+        mealName: null,
+        ingredients: [],
+        mealimage: "/images/black.jpg"
     }
 
-    componentDidMount() {
+    // componentDidMount() {
+    //     fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data)
+    //             this.setState({ 
+    //                 recipe: data.meals[0],
+    //                 mealName: data.meals[0].strMeal,
+    //                 mealimage: data.meals[0].strMealThumb
+    //             });
+
+    //             let ingredients = []
+    //             console.log(this.state.recipe);
+    //             console.log(this.state.recipe.strMeal);
+    //             console.log(this.state.mealName);
+    //         }
+    //         )
+    // };
+
+    fetchMeal() {
         fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                this.setState({ recipe: data });
-            }
-            )
-    };
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            this.setState({ 
+                recipe: data.meals[0],
+                mealName: data.meals[0].strMeal,
+                mealimage: data.meals[0].strMealThumb
+            });
+            console.log(this.state.recipe);
+            console.log(this.state.recipe.strMeal);
+            console.log(this.state.mealName);
+        }
+        )
+    }
 
     render() {
         return (
@@ -24,10 +53,11 @@ class Home extends Component {
                         <h2>Welcome to the random meal generator</h2>
                         <p>Have a hungry family to feed but don't know what to make for dinner?</p>
                         <p>Simply give our random meal generator button a click and prepare to be inspired!</p>
-                        <button className="btn btn-danger btn-lg">Get my meal</button>
+                        <button className="btn btn-danger btn-lg" onClick={() => this.fetchMeal()}>Get my meal</button>
                     </div>
                     <div id="meal-search-result">
-                        {this.state.recipe}
+                        <h2>{this.state.mealName}</h2>
+                        <img src={this.state.mealimage} alt="meal image" />
                     </div>
                 </div>
             </div>
