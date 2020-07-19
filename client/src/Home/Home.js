07f3c8fc7da16fbed5d9a4eec3842ea0d091b85e
Dane M Goodman <DaneGoodman85@gmail.com>
2020-07-19 13:51:53 -0700

    The entire random meal generator functionality is complete, the only thing missing is a new font from font-awesome. After that I will be moving on to the backend portion, creating functionality to archive recipes that I like

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
 import './Home.css';
 
 class Home extends Component {
@@ -15,49 +16,49 @@ class Home extends Component {
     //Fetch data from meal API to set to state when button is clicked
     fetchMeal() {
         fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
 
-            //Create array of ingredients to set to state
-            let ingredientsArray = [];
-            for (let i = 1;  i < 20; i++) {
-                if (data.meals[0][`strIngredient${i}`] === "" || data.meals[0][`strIngredient${i}`] === null) {
-                    console.log("empty ingredient string");
+                //Create array of ingredients to set to state
+                let ingredientsArray = [];
+                for (let i = 1; i < 20; i++) {
+                    if (data.meals[0][`strIngredient${i}`] === "" || data.meals[0][`strIngredient${i}`] === null) {
+                        console.log("empty ingredient string");
+                    }
+                    else {
+                        ingredientsArray.push(data.meals[0][`strIngredient${i}`]);
+                    }
+                };
+
+                //Create array of measurements to set to state
+                let measureArray = [];
+                for (let i = 1; i < 20; i++) {
+                    if (data.meals[0][`strMeasure${i}`] === "" || data.meals[0][`strMeasure${i}`] === null) {
+                        console.log("empty measurement string");
+                    }
+                    else {
+                        measureArray.push(data.meals[0][`strMeasure${i}`]);
+                    }
+
                 }
-                else {
-                    ingredientsArray.push(data.meals[0][`strIngredient${i}`]);
-                }
-            };
 
-             //Create array of measurements to set to state
-             let measureArray = [];
-             for (let i = 1;  i < 20; i++) {
-                 if (data.meals[0][`strMeasure${i}`] === "" || data.meals[0][`strMeasure${i}`] === null) {
-                     console.log("empty measurement string");
-                 }
-                 else {
-                     measureArray.push(data.meals[0][`strMeasure${i}`]);
-                 }
- 
-             }
+                console.log(measureArray);
 
-            console.log(measureArray);
-
-            this.setState({ 
-                recipe: data.meals[0],
-                mealName: data.meals[0].strMeal,
-                ingredients: ingredientsArray,
-                measurements: measureArray,
-                mealimage: data.meals[0].strMealThumb,
-                instructions: data.meals[0].strInstructions,
-                youtube: data.meals[0].strYoutube
-            });
-            console.log(this.state.recipe);
-            console.log(this.state.recipe.strMeal);
-            console.log(this.state.mealName);
-        }
-        )
+                this.setState({
+                    recipe: data.meals[0],
+                    mealName: data.meals[0].strMeal,
+                    ingredients: ingredientsArray,
+                    measurements: measureArray,
+                    mealimage: data.meals[0].strMealThumb,
+                    instructions: data.meals[0].strInstructions,
+                    youtube: data.meals[0].strYoutube
+                });
+                console.log(this.state.recipe);
+                console.log(this.state.recipe.strMeal);
+                console.log(this.state.mealName);
+            }
+            )
     }
 
 
@@ -74,17 +75,23 @@ class Home extends Component {
         return ingredients;
     }
 
-    
+
 
     render() {
         return (
             <div>
                 <div className="flex-container">
                     <div id="meal-search-content">
-                        <h2>Welcome to the random meal generator</h2>
-                        <p>Have a hungry family to feed but don't know what to make for dinner?</p>
-                        <p>Simply give our random meal generator button a click and prepare to be inspired!</p>
-                        <button className="btn btn-danger btn-lg" onClick={() => this.fetchMeal()}>Get my meal</button>
+                        <div id="content-margin">
+                            <img src='/images/eat.jpg' alt='logo' id='content-logo' />
+                            <h2>Welcome to the random meal generator</h2>
+                            <p>Have a hungry family to feed but don't know what to make for dinner?</p>
+                            <p>Simply give our random meal generator button a click and prepare to be inspired!</p>
+                            <button className="btn btn-danger btn-lg" onClick={() => this.fetchMeal()}>WHAT'S FOR DINNER?</button>
+                            <br></br>
+                            <button className="btn btn-danger btn-lg" id="save-button">SAVE THIS RECIPE</button>
+
+                        </div>
                     </div>
                     <div id="meal-search-result">
                         <h2>{this.state.mealName}</h2>
@@ -93,7 +100,10 @@ class Home extends Component {
                             {this.renderIngredients()}
                         </ol>
                         <p id="instructions">{this.state.instructions}</p>
-                        <iframe width="560" height="315" src={this.state.youtube} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <ReactPlayer
+                            width={400}
+                            url={this.state.youtube}
+                        />
                     </div>
                 </div>
             </div>
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,7 +5,19 @@ const Header = () => {
     return (
         <div id="header">
             <div id="header-content">
-                <img src="./images/eat.jpg" alt="eat it" id="eat" />
+                <div className="row">
+                    <div className="col-4">
+                        <h3>Dane's Random Meal Generator</h3>
+                    </div>
+                    <div className="col-4">
+                        <img src="./images/eat.jpg" alt="eat it" id="eat" />
+                    </div>
+                    <div className="col-4">
+                        <h5>MY SAVED MEALS</h5>
+                    </div>
+                </div>
+                {/* <h3>Dane's Random Meal Generator</h3>
+                <img src="./images/eat.jpg" alt="eat it" id="eat" /> */}
             </div>
         </div>
     )
